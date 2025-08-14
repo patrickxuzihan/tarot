03c7d8b8298d0a9c57ef9fc58a125a4f90e16313
patrickxuzihan <zxu536@gmail.com>
2025-08-14 18:24:51 +0800

    注册新页面

--- Frontend/Tarot_Android/HomeView.js
+++ Frontend/Tarot_Android/HomeView.js
@@ -1,3 +1,4 @@
+// 各种库
 import React from 'react';
 import { BackHandler, StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -6,16 +7,52 @@ import { Ionicons } from '@expo/vector-icons';
 import { useFocusEffect } from '@react-navigation/native';
 import { useAppTheme } from './Account/Setup/ThemesHelper';
 
-import QuickDivinationView from './Oracle/QuickDivinationView';
+// HomeScreen子页面
+import PlayerView from './Player/PlayerView';
+
+// ForumScreen子页面
 import NotificationsView from './Notifications/NotificationsView';
 import NewPostView from './Forum/NewPostView';
 import PostDetailView from './Forum/PostDetailView';
-import PlayerView from './Player/PlayerView';
+import NotificationsDetailView from './Notifications/NotificationsDetailView';
+
+// TarotEnergy子页面
+import QuickDivinationView from './Oracle/QuickDivinationView';
 import PrivateDivinationView from './TarotEnergy/PrivateDivinationView';
 import DailyTopicsView from './TarotEnergy/DailyTopicsView';
+import CardLibraryView from './TarotEnergy/CardLibraryView';
+import ReadingHistoryView from './TarotEnergy/ReadingHistoryView'
+import StatsInsightsView from './TarotEnergy/StatsInsightsView'
+import EnergyNotesView from './TarotEnergy/EnergyNotesView'
+import HoroscopeView from './TarotEnergy/HoroscopeView';
+import TarotKnowledgeView from './TarotEnergy/TarotKnowledgeView';
+import TarotTutorialView from './TarotEnergy/TarotTutorialView';
+
+// TarotHouse子页面
+
+// Account子页面
 import SettingsView from './Account/SettingsView';
 import ThemesSettingsView from './Account/Setup/ThemesSettingsView';
+import ProfileEditorView from './Account/Setup/ProfileEditorView';
+import SecuritySettingsView from './Account/Setup/SecuritySettingsView';
+import EmailBindingView from './Account/Setup/EmailBindingView';
+import PasswordChangeView from './Account/Setup/PasswordChangeView';
+import HelpCenterView from './Account/Setup/HelpCenterView';
+import AboutAppView from './Account/Setup/AboutAppView';
+import PrivacyPolicyView from './Account/Setup/PrivacyPolicyView';
+import TermsOfServiceView from './Account/Setup/TermOfServiceView';
+import NotificationSettingsView from './Account/Setup/NotificationSettingsView';
+import FollowersView from './Account/FollowersView';
+import MembershipView from './Account/MembershipView';
+import DailySignView from './Account/DailySignView';
+import WithdrawalView from './Account/WithdrawalView';
+import OutfitView from './Account/OutfitView';
+import CustomizationView from './Account/CustomizationView';
+import DownloadsView from './Account/DownloadsView';
+import FavoritesView from './Account/FavoritesView'
+import SubscriptionsView from './Account/SubscriptionsView'
 
+// Tab管理的五个Screen
 import HomeScreen from './HomeScreen';
 import ForumScreen from './Forum/ForumView';
 import TarotEnergyScreen from './TarotEnergy/TarotEnergyView';
@@ -59,6 +96,7 @@ function ForumStackScreen() {
       <ForumStack.Screen name="Notifications" component={NotificationsView} />
       <ForumStack.Screen name="NewPost" component={NewPostView} />
       <ForumStack.Screen name="PostDetail" component={PostDetailView} />
+      <ForumStack.Screen name="NotificationsDetail" component={NotificationsDetailView} />
     </ForumStack.Navigator>
   );
 }
@@ -77,6 +115,13 @@ function TarotEnergyStackScreen() {
       <TarotEnergyStack.Screen name="QuickDivination" component={QuickDivinationView} />
       <TarotEnergyStack.Screen name="DailyTopics" component={DailyTopicsView} />
       <TarotEnergyStack.Screen name="PrivateDivination" component={PrivateDivinationView} />
+      <TarotEnergyStack.Screen name="CardLibrary" component={CardLibraryView} />
+      <TarotEnergyStack.Screen name="ReadingHistory" component={ReadingHistoryView} />
+      <TarotEnergyStack.Screen name="StatsInsights" component={StatsInsightsView} />
+      <TarotEnergyStack.Screen name="EnergyNotes" component={EnergyNotesView} />
+      <TarotEnergyStack.Screen name="Horoscope" component={HoroscopeView} />
+      <TarotEnergyStack.Screen name="TarotKnowledge" component={TarotKnowledgeView} />
+      <TarotEnergyStack.Screen name="TarotTutorial" component={TarotTutorialView} />
     </TarotEnergyStack.Navigator>
   );
 }
@@ -109,6 +154,24 @@ function AccountStackScreen() {
       <AccountStack.Screen name="Account" component={AccountScreen} />
       <AccountStack.Screen name="Settings" component={SettingsView} />
       <AccountStack.Screen name="ThemesSettings" component={ThemesSettingsView} />
+      <AccountStack.Screen name="ProfileEditor" component={ProfileEditorView} />
+      <AccountStack.Screen name="SecuritySettings" component={SecuritySettingsView} />
+      <AccountStack.Screen name="EmailBinding" component={EmailBindingView} />
+      <AccountStack.Screen name="PasswordChange" component={PasswordChangeView} />
+      <AccountStack.Screen name="HelpCenter" component={HelpCenterView} />
+      <AccountStack.Screen name="AboutApp" component={AboutAppView} />
+      <AccountStack.Screen name="PrivacyPolicy" component={PrivacyPolicyView} />
+      <AccountStack.Screen name="TermsOfService" component={TermsOfServiceView} />
+      <AccountStack.Screen name="NotificationSettings" component={NotificationSettingsView} />
+      <AccountStack.Screen name="Followers" component={FollowersView} />
+      <AccountStack.Screen name="Membership" component={MembershipView} />
+      <AccountStack.Screen name="DailySign" component={DailySignView} />
+      <AccountStack.Screen name="Withdrawal" component={WithdrawalView} />
+      <AccountStack.Screen name="Outfit" component={OutfitView} />
+      <AccountStack.Screen name="Customization" component={CustomizationView} />
+      <AccountStack.Screen name="Downloads" component={DownloadsView} />
+      <AccountStack.Screen name="Favorites" component={FavoritesView} />
+      <AccountStack.Screen name="Subscriptions" component={SubscriptionsView} />
     </AccountStack.Navigator>
   );
 }
